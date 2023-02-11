b5275aa63eb4df229e09939aab358d3016994e79
ManolisManouselis <78966297+ManolisManouselis@users.noreply.github.com>
2023-02-11 10:59:39 +0200

    Update covid_webscraping.py

--- lab1/lab1b/covid_webscraping.py
+++ lab1/lab1b/covid_webscraping.py
@@ -1,48 +1,71 @@
-from bs4 import BeautifulSoup
-import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 url = ("https://covid19.who.int/table")
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
 
-html = requests.get(url).text
-soup = BeautifulSoup(html, 'html.parser') #text and the parser to use  CHANGE from "html5lib" to "html.parser"
+driver = webdriver.Chrome(chrome_options=options)
+wait = WebDriverWait(driver, 10)
 
-covid = soup.find('div', attrs={'role':'table'})
+driver.get(url)
+#  To scroll down table in order to get all records - It doesn't work
+for i in range(5):
+    inner_scroll_bar = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "tbody")))
+    # element = driver.find_element_by_id('gatsby-focus-wrapper')
+    scroll_top = 12000
+    driver.execute_script("arguments[0].scrollTop = arguments[1]", inner_scroll_bar)
+    inner_scroll_contents = inner_scroll_bar.text
 
 
-# List for Headers
-headers_first = covid.find('div', attrs={'class':"sc-AxjAm kbGAkV"})
-headers_list_first = [row.get_text(strip = True) for row in headers_first]
-headers = covid.find_all('div', attrs={'class':"sc-pcJja jjDvpo"})
-headers_list_second = [row.get_text(strip = True) for row in headers]
 
-headers_list = headers_list_first + headers_list_second
-headers_list.remove('')
+# Path for Headers / Totals
+temp_list =[]
 
-# List for Totals
-totals = covid.find_all('div', attrs={'data-id':"totals-header"})
-totals_list = [row.get_text(strip = True) for row in totals]
+elements = driver.find_elements(By.CLASS_NAME, "th")
+
+for item in elements:
+    temp_list.append(item.text)
+
+
+# Split headers / totals
+
+totals_list = []
+totals_list = temp_list[8:]
+headers_list = temp_list[:8]
+
+
 
 # List for Rows
-rows = covid.find_all('div', class_=lambda class_: class_ and ('sc-AxjAm sc-pliRl ylFcR' in class_ or 'sc-pXZzD fMTLAh' in class_))
-rows_list = [row.get_text(strip = True) for row in rows]
 
-# For first 2 rows, one value is missing
-row_list = rows_list.insert( 7, '')
-row_list = rows_list.insert( 15, '')
 
-# Slice rows list of 158 values, into sublists of 8 values
+elements = driver.find_elements(By.CLASS_NAME, "td")
+rows_list = []
+
+for item in elements:
+    rows_list.append(item.text)
+
+driver.quit()
+
+# Slice rows list of 160 values, into sublists of 8 values
 sublist_size = 8
 row_list_2 =  [rows_list[i:i+sublist_size] for i in range(0, len(rows_list), sublist_size)]
 
 
-row_list_3 = row_list_2.copy()
-row_list_3 = [[value.replace(",", "") for value in sublist] for sublist in row_list_3]
 
 # turn measures to float from strings
+
+row_list_3 = row_list_2.copy()
+row_list_3 = [[value.replace(",", "") for value in sublist] for sublist in row_list_3]
 for i in range(2,20):
     for j in range(1,8):
         if row_list_3[i][j] != "":
             row_list_3[i][j] = float(row_list_3[i][j])
+
 
 
 # Create dictionary based on Header - Rows
