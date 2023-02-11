f76c48ce660c37ee9194145aecdd99e223e25604
ManolisManouselis <78966297+ManolisManouselis@users.noreply.github.com>
2023-02-11 10:59:55 +0200

    Update happiness_webscraping.py

--- lab1/lab1b/happiness_webscraping.py
+++ lab1/lab1b/happiness_webscraping.py
@@ -1,43 +1,69 @@
-from bs4 import BeautifulSoup
-import requests
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import pandas as pd
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
 
 url = 'https://www.kaggle.com/unsdsn/world-happiness?select=2019.csv'
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
+
 driver = webdriver.Chrome(chrome_options=options)
+wait = WebDriverWait(driver, 40)
 
 driver.get(url)
 
-page = driver.page_source
-# driver.quit()
-
-soup = BeautifulSoup(page, 'html.parser')
-
-header = soup.find_all('span', attrs={'class':'sc-ikZpkk sc-jIZahH sc-iDiDda fEheNQ fUZPns kwoekg'})
-headers_list = [row.get_text(strip = True) for row in header]
+#  To scroll down table in order to get all records
+for i in range(10):
+    inner_scroll_bar = wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/main/div[1]/div[1]/div[6]/div[1]/div[1]/div[5]/div[2]/div[1]/div[1]/div[1]/div[1]/div[3]")))
+    driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", inner_scroll_bar)
+    inner_scroll_contents = inner_scroll_bar.text
 
 
-text = []
-elements = driver.find_elements("xpath", "//div[@class='sc-KpIxo sc-jPgKOH jZTfen fbMUHr']")
-for item in elements:
-    text.append(item.text)
+
+# Path for Headers
+
+path = "/html/body/main/div[1]/div[1]/div[6]/div[1]/div[1]/div[5]/div[2]/div[1]/div[1]/div[1]/div[1]/div[3]/div[4]/div[1]"
+elements = driver.find_elements("xpath", path)
+
+
+# Create list with Headers by finding specific span element
+headers_list = []
+for element in elements:
+    spans = element.find_elements("xpath", ".//span")
+    for span in spans:
+        # check if the span has the desired text
+        if span.text != "":
+            headers_list.append(span.text)
+
+
+# Find path for rows
+# For each span element which contains one row of table, add it to a list
+rows = []
+for i in range(1,160):
+    path = "/html/body/main/div[1]/div[1]/div[6]/div[1]/div[1]/div[5]/div[2]/div[1]/div[1]/div[1]/div[1]/div[3]/div[7]/span[2]"
+    path = path[:-2] + str(i) +  "]"
+    elements = driver.find_elements("xpath", path)
+    for item in elements:
+        rows.append(item.text)
+        index = i
+
+
 driver.quit()
+
 
 
 # Replace all occurences of \n with ,
 row2 = []
-for element in text:
+for element in rows:
     row2.append(element.replace("\n", ","))
 
 
-# Its a list of index 50, Split it into 50 sublists of 9 values based on comma
+# Its a list of 156 strings, Split it into 156 sublists of 9 values based on comma
 
-row_list = [[] for _ in range(50)]
+row_list = [[] for _ in range(index)]
 
 
 for index, i in enumerate(row2):
@@ -57,6 +83,13 @@ for index, i in enumerate(row2):
             strng = strng + j
 
 
+# turn measures to float from strings
+for i in range(index):
+    for j in range(2,9):
+        if row_list[i][j] != "":
+            row_list[i][j] = float(row_list[i][j])
+
+#  Create dictionary based on headers, rows
 happiness_dict = [dict(zip(headers_list, row)) for row in row_list]
 
 
