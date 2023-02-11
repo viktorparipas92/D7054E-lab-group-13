76a04c24e3c321a80e4be8d86c396df82aef05c3
Paripás Viktor <viktor.paripas@kognity.com>
2023-02-11 20:24:48 +0100

    Disable lab1b.py as a script

--- lab1/lab1b/lab1b.py
+++ lab1/lab1b/lab1b.py
@@ -23,18 +23,3 @@ def get_as_dict(url, user=None, password=None):
 def get_raw_text(url, user=None, password=None):
     response = get(url, user, password)
     return response.text
-
-
-if __name__ == '__main__':
-    COVID_URL = 'https://api.covid19api.com/summary'
-    response_dict = get_as_dict(COVID_URL)
-    for country_dict in response_dict['Countries']:
-        CovidData(
-            name=country_dict['Country'],
-            region=...,
-            total_cases=country_dict['TotalConfirmed'],
-            total_deaths=country_dict['TotalDeaths'],
-
-        )
-
-
