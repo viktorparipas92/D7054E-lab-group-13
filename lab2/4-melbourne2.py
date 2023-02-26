286e5bf4b72e8524972310bd16f45768604fb3d3
Paripás Viktor <viktor.paripas@kognity.com>
2023-02-26 16:27:30 +0100

    Rename melbourne 2 and fix formatting errors

--- lab2/4-melbourne2.py
+++ lab2/4-melbourne2.py
@@ -3,22 +3,29 @@ from matplotlib import pyplot as plt
 import seaborn as sns
 
 
-
 def plot_distance_price():
     # Task 1: Correlation between distance from CBD and prices of properties
-    sns.regplot(x="Distance", y="Price", data=melbourne_data)
-    plt.title("Distance vs Price")
+    sns.regplot(x='Distance', y='Price', data=melbourne_data)
+    plt.title('Distance vs Price')
     plt.show()
 
 
 def plot_council_year():
-    council_year = melbourne_data.groupby(['CouncilArea', 'YearBuilt']).size().reset_index( name='count' )
+    council_year = melbourne_data.groupby(
+        ['CouncilArea', 'YearBuilt']
+    ).size().reset_index(name='count')
     council_year = council_year[council_year['YearBuilt'] >= 1800]
 
-    top10_councils = council_year.groupby('CouncilArea')['count'].sum().nlargest(10).index
-    council_year = council_year[council_year['CouncilArea'].isin(top10_councils)]
+    top10_councils = council_year.groupby(
+        'CouncilArea'
+    )['count'].sum().nlargest(10).index
+    council_year = council_year[
+        council_year['CouncilArea'].isin(top10_councils)
+    ]
 
-    sns.scatterplot(x='YearBuilt', y='count', hue='CouncilArea', data=council_year)
+    sns.scatterplot(
+        x='YearBuilt', y='count', hue='CouncilArea', data=council_year
+    )
     plt.xlabel('Year Built')
     plt.ylabel('Number of Properties')
     plt.title('Number of Properties Built by Year and Council (Top 10)')
@@ -26,13 +33,22 @@ def plot_council_year():
 
 
 def plot_seller_location():
-    top_sellers = melbourne_data['SellerG'].value_counts().nlargest(5).index.tolist()
+    top_sellers = (
+        melbourne_data['SellerG'].value_counts()
+        .nlargest(5)
+        .index
+        .tolist()
+    )
     seller_data = melbourne_data[melbourne_data['SellerG'].isin(top_sellers)]
 
-    plt.figure(figsize=(10,8))
+    plt.figure(figsize=(10, 8))
     for i, seller in enumerate(top_sellers):
         seller_subset = seller_data[seller_data['SellerG'] == seller]
-        plt.scatter(seller_subset['Longtitude'], seller_subset['Lattitude'], alpha=0.5, label=seller, cmap='viridis')
+        plt.scatter(
+            seller_subset['Longtitude'],
+            seller_subset['Latitude'],
+            alpha=0.5, label=seller, cmap='viridis',
+        )
 
     plt.xlabel('Longitude')
     plt.ylabel('Latitude')
@@ -40,11 +56,9 @@ def plot_seller_location():
     plt.legend()
     plt.show()
 
+
 if __name__ == '__main__':
     melbourne_data = pd.read_csv('datasets/melbourne_data.csv')
-
     plot_distance_price()
-
     plot_council_year()
-
     plot_seller_location()
