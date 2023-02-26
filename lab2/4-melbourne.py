adc81980e6c1e6a76bed361a7b09e1e276d81dfc
Paripás Viktor <viktor.paripas@kognity.com>
2023-02-26 16:41:46 +0100

    Refactor melbourne to use create_plots and save to file

--- lab2/4-melbourne.py
+++ lab2/4-melbourne.py
@@ -1,58 +1,57 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 
-
-def plot_relationship_between_price_and_size():
-    plt.scatter(
-        melbourne_data['Rooms'], melbourne_data['Price']
-    )
-    plt.xlabel('Number of rooms')
-    plt.ylabel('Real estate price')
-    plt.show()
-    plt.scatter(
-        melbourne_data['Bedroom2'], melbourne_data['Price']
-    )
-    plt.xlabel('Number of bedrooms')
-    plt.ylabel('Real estate price')
-    plt.show()
-    plt.scatter(
-        melbourne_data['Bedroom2'] + melbourne_data['Bathroom'],
-        melbourne_data['Price']
-    )
-    plt.xlabel('Sum of number of bath- and bedrooms')
-    plt.ylabel('Real estate price')
-    plt.show()
+from plot import create_plots
 
 
-def plot_property_counts_by_suburb():
+def _plot_relationship_between_price_and_size(melbourne_data, variant=2):
+    if variant == 0:
+        x = melbourne_data['Rooms']
+        plt.xlabel('Number of rooms')
+    elif variant == 1:
+        x = melbourne_data['Bedroom2']
+        plt.xlabel('Number of bedrooms')
+    elif variant == 2:
+        x = melbourne_data['Bedroom2'] + melbourne_data['Bathroom']
+        plt.xlabel('Sum of number of bath- and bedrooms')
+    else:
+        return
+
+    plt.scatter(x, melbourne_data['Price'])
+    plt.ylabel('Real estate price')
+
+
+def _plot_property_counts_by_suburb(melbourne_data):
     property_counts = melbourne_data.groupby(['Suburb']).count()[
         'Propertycount'].sort_values(ascending=False)
-    property_counts.plot.bar()
+    # property_counts.plot.bar()
     plt.xlabel('Suburbs')
     plt.ylabel('Property counts')
     plt.xticks([], [])
-    plt.show()
+
     labels = ['' for x in property_counts]
-    print(property_counts.head())
     num_suburb_names_to_show = 10
     labels[:num_suburb_names_to_show] = property_counts.index[
                                         :num_suburb_names_to_show]
     property_counts.plot.pie(labels=labels)
-    plt.show()
 
 
-if __name__ == '__main__':
-    melbourne_data = pd.read_csv('datasets/melbourne_data.csv')
-
-    # Correlation between price and distance
+def _plot_correlation_between_distance_and_price(melbourne_data):
     correlation = melbourne_data['Price'].corr(melbourne_data['Distance'])
     print(
         f"Correlation between the price and distance columns "
         f"is {correlation:.3f}."
     )
 
-    # Scatter plot between size and price
-    # plot_relationship_between_price_and_size()
 
-    # Number of real estates per suburb
-    plot_property_counts_by_suburb()
+PLOT_TO_FILE_MAP = {
+    _plot_correlation_between_distance_and_price: 'melbourne-corr-distance.png',
+    _plot_property_counts_by_suburb: 'melbourne-properties-per-suburb.png',
+    _plot_relationship_between_price_and_size: 'melbourne-scatter-size.png'
+}
+
+
+if __name__ == '__main__':
+    melbourne_data = pd.read_csv('datasets/melbourne_data.csv')
+
+    create_plots(melbourne_data, PLOT_TO_FILE_MAP, save_to_file=True)
