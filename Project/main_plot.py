f54fdea2b5e8a6db83abba198b3959de79cb80de
Paripás Viktor <viktor.paripas@kognity.com>
2023-03-17 11:01:49 +0100

    Inject dependency explicitly

--- Project/main_plot.py
+++ Project/main_plot.py
@@ -1,21 +1,21 @@
 import importlib
 from attrition_api import fetch_data
 
-# Import all functions from attrition_plots
-attrition_plots = importlib.import_module("attrition_plots")
-all_functions = [
-    func for func in dir(attrition_plots)
-    if callable(getattr(attrition_plots, func))
-    and not func.startswith("__")
-]
 
-
-def generate_visualizations(data):
-    for func_name in all_functions:
+def generate_visualizations(functions, data):
+    for func_name in functions:
         func = getattr(attrition_plots, func_name)
         func(data)
 
 
 if __name__ == '__main__':
+    attrition_plots = importlib.import_module('attrition_plots')
+    all_functions = [
+        func for func in dir(attrition_plots)
+        if callable(getattr(attrition_plots, func))
+        and not func.startswith('__')
+    ]
+
     attrition_df = fetch_data()
-    generate_visualizations(attrition_df)
+
+    generate_visualizations(all_functions, attrition_df)
