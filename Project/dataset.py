68d90d2a77aba520a19c442f50fa6b5611e12df8
Paripás Viktor <viktor.paripas@kognity.com>
2023-03-18 14:00:24 +0100

    Refactor dataset.py and add some preprocessing

--- Project/dataset.py
+++ Project/dataset.py
@@ -1,3 +1,6 @@
+from typing import Tuple
+
+import numpy as np
 import pandas as pd
 
 
@@ -8,27 +11,74 @@ FEATURES_TO_REMOVE = [
     'StandardHours',
 ]
 
+NUMERICAL_CATEGORICAL_FEATURES = [
+    'Education',
+    'EnvironmentSatisfaction',
+    'JobInvolvement',
+    'JobLevel',
+    'JobSatisfaction',
+    'PerformanceRating',
+    'RelationshipSatsifaction',
+    'WorkLifeBalance',
+    'StockOptionLevel',
+]
+
+TARGET = ['Attrition']
+
+
+def split_into_categorical_and_numerical_data(dataset) -> Tuple[
+    pd.DataFrame, pd.DataFrame
+]:
+    all_features = dataset.columns.tolist()
+    numerical_features = dataset.select_dtypes(
+        include=[np.int64, np.float64]
+    ).columns.tolist()
+
+    numerical_features = list(
+        set(numerical_features) - set(NUMERICAL_CATEGORICAL_FEATURES)
+    )
+    categorical_features = list(
+        set(all_features) - set(numerical_features) - set(TARGET)
+    )
+
+    return dataset[categorical_features], dataset[numerical_features]
+
+
+def transform_numerical_features(numerical_features):
+    column_names = numerical_features.columns.tolist()
+    for column in column_names:
+        if numerical_features[column].skew() > 0.80:
+            numerical_features[column] = np.log1p(numerical_features[column])
+
+
+def transform_categorical_features(categorical_features):
+    column_names = categorical_features.columns.tolist()
+    for col in column_names:
+        column_dummies = pd.get_dummies(categorical_features[col], prefix=col)
+        categorical_features = pd.concat(
+            [categorical_features, column_dummies], axis=1
+        )
+    categorical_features.drop(column_names, axis=1, inplace=True)
+    return categorical_features
+
+
+def transform_target(dataset):
+    attrition_target = dataset['Attrition'].map({'Yes': 1, 'No': 0})
+    return attrition_target
+
 
 def preprocess_attrition_data(dataset):
-    # Convert Attrition column to binary 0/1
-    dataset['Attrition'] = (dataset['Attrition'] == 'Yes').astype(int)
-
-    categorical_variables = [
-        'BusinessTravel',
-        'Department',
-        'EducationField',
-        'Gender',
-        'JobRole',
-        'MaritalStatus',
-        'OverTime',
-    ]
-    for column_name in categorical_variables:
-        cat_list = pd.get_dummies(dataset[column_name], prefix=column_name)
-        dataset = dataset.join(cat_list)
-
-    # Create a list of independent variables ignoring some
-    labels = dataset['Attrition']
+    dataset = dataset.copy()
+    (
+        categorical_data,
+        numerical_data,
+    ) = split_into_categorical_and_numerical_data(dataset)
     dataset.drop(FEATURES_TO_REMOVE, axis=1, inplace=True)
-    dataset.drop(categorical_variables, axis=1, inplace=True)
-    features = dataset.drop('Attrition', axis=1)
-    return features, labels
+
+    transform_numerical_features(numerical_data)
+    categorical_data = transform_categorical_features(categorical_data)
+    final_dataset = pd.concat([numerical_data, categorical_data], axis=1)
+
+    attrition_target = transform_target(dataset)
+
+    return final_dataset, attrition_target
